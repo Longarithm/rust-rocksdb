1cf2b4f94e2a279b110d977c33a72fa7b59dcb4a
Longarithm <the.aleksandr.logunov@gmail.com>
2021-12-22 03:23:44 +0300

    update branch correctly

--- librocksdb-sys/build_version.cc
+++ librocksdb-sys/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@849c295cbf9225ec11acc3aee36f823104920b96@";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@30a574b298f079c415ddbea30433300fdda5a464@";
 const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2021/02/18 09:36:23@";
 const char* rocksdb_build_compile_date = __DATE__;
